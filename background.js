7aabf5ba3847728de36e25cec05eefda6820f0aa
Unknown <dev@gustiaux.com>
2017-11-25 17:50:07 -0500

    Fix ESLint errors and warnings

--- background.js
+++ background.js
@@ -4,15 +4,15 @@
  * ================================================================================
  */
 
-const OPTIONS_FOLDER = "folder";
-const OPTIONS_OVERRIDE = "override";
-const OPTIONS_ICON = "icon";
-const OPTIONS_INBOX = "inbox";
-const OPTIONS_ADDTOTOP = "addtotop";
+const OPTIONS_FOLDER = 'folder'
+const OPTIONS_OVERRIDE = 'override'
+const OPTIONS_ICON = 'icon'
+const OPTIONS_INBOX = 'inbox'
+const OPTIONS_ADDTOTOP = 'addtotop'
 
-const FOLDER_NONE = "none";
+const FOLDER_NONE = 'none'
 
-const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_INBOX, OPTIONS_ADDTOTOP];
+const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_INBOX, OPTIONS_ADDTOTOP]
 
 /*
  * ================================================================================
@@ -23,54 +23,54 @@ const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_I
 /*
  * Indicates if the override settings has been enabled and a folder selected
  */
-function isNewLocationSet(options) {
-  var isSet = false;
+function isNewLocationSet (options) {
+  let isSet = false
   if (options.hasOwnProperty(OPTIONS_OVERRIDE) && options[OPTIONS_OVERRIDE] === true) {
     if (options[OPTIONS_FOLDER] !== undefined && options[OPTIONS_FOLDER] !== FOLDER_NONE) {
-      isSet = true;
+      isSet = true
     }
   }
-  return isSet;
+  return isSet
 }
 
 /*
  * Indicates if the "quick bookmark" icon has been enabled
  */
-function quickBookmarkMode(options) {
-  var isEnabled = false;
+function quickBookmarkMode (options) {
+  let isEnabled = false
   if (options.hasOwnProperty(OPTIONS_ICON) && options[OPTIONS_ICON] === true) {
-    isEnabled = true;
+    isEnabled = true
   }
-  return isEnabled;
+  return isEnabled
 }
 
 /*
  * Indicates if the "inbox mode" has been enabled
  */
-function inboxMode(options) {
-  var isEnabled = false;
+function inboxMode (options) {
+  let isEnabled = false
   if (options.hasOwnProperty(OPTIONS_INBOX) && options[OPTIONS_INBOX] === true) {
-    isEnabled = true;
+    isEnabled = true
   }
-  return isEnabled;
+  return isEnabled
 }
 
 /*
  * Indicates if the "add to top of folder mode" has been enabled
  */
-function addToTopMode(options) {
-  var isEnabled = false;
+function addToTopMode (options) {
+  let isEnabled = false
   if (options.hasOwnProperty(OPTIONS_ADDTOTOP) && options[OPTIONS_ADDTOTOP] === true) {
-    isEnabled = true;
+    isEnabled = true
   }
-  return isEnabled;
+  return isEnabled
 }
 
 /*
  * Logs errors to the console
  */
-function onError(error) {
-  console.log(`An error occurred: ${error}`);
+function onError (error) {
+  console.log(`An error occurred: ${error}`)
 }
 
 /*
@@ -82,31 +82,29 @@ function onError(error) {
 /*
  * Moves the bookmark to the specified folder (if option activated)
  */
-function handleCreated(id, bookmarkInfo) {
+function handleCreated (id, bookmarkInfo) {
   // Only process bookmarks (not folders or separators) with an actual URL
-  if (bookmarkInfo.type === "bookmark" && bookmarkInfo.hasOwnProperty("url")) {
-    if (bookmarkInfo.url !== undefined && bookmarkInfo.url !== "about:blank") {
-
-      var gettingOptions = browser.storage.local.get(OPTIONS_ARRAY);
+  if (bookmarkInfo.type === 'bookmark' && bookmarkInfo.hasOwnProperty('url')) {
+    if (bookmarkInfo.url !== undefined && bookmarkInfo.url !== 'about:blank') {
+      let gettingOptions = browser.storage.local.get(OPTIONS_ARRAY)
       gettingOptions.then((options) => {
-
-        var bookmarkTreeNode = {};
+        let bookmarkTreeNode = {}
 
         if (isNewLocationSet(options)) {
-          bookmarkTreeNode.parentId = options[OPTIONS_FOLDER];
+          bookmarkTreeNode.parentId = options[OPTIONS_FOLDER]
         }
         if (addToTopMode(options)) {
-          bookmarkTreeNode.index = 0;
+          bookmarkTreeNode.index = 0
         }
 
-        browser.bookmarks.move(id, bookmarkTreeNode);
-      }, onError);
+        browser.bookmarks.move(id, bookmarkTreeNode)
+      }, onError)
     }
   }
 }
 
 // Listen for bookmarks being created
-browser.bookmarks.onCreated.addListener(handleCreated);
+browser.bookmarks.onCreated.addListener(handleCreated)
 
 /*
  * ================================================================================
@@ -114,140 +112,139 @@ browser.bookmarks.onCreated.addListener(handleCreated);
  * ================================================================================
  */
 
-var currentTab;
-var currentBookmark;
+let currentTab
+let currentBookmark
 
 /*
  * Updates the browserAction icon to reflect whether the current page
  * is already bookmarked.
  */
-function updateIcon(iconEnabled) {
+function updateIcon (iconEnabled) {
   if (iconEnabled === true) {
     browser.pageAction.setIcon({
       path: currentBookmark ? {
-        32: "icons/star-button-32.png",
-        48: "icons/star-button-48.png"
+        32: 'icons/star-button-32.png',
+        48: 'icons/star-button-48.png'
       } : {
-        32: "icons/star-button-empty-32.png",
-        48: "icons/star-button-empty-48.png"
+        32: 'icons/star-button-empty-32.png',
+        48: 'icons/star-button-empty-48.png'
       },
       tabId: currentTab.id
-    });
+    })
     browser.pageAction.setTitle({
       // Screen readers can see the title
       title: currentBookmark ? 'Unbookmark it!' : 'Bookmark it!',
       tabId: currentTab.id
-    });
+    })
   } else {
     browser.pageAction.setIcon({
       path: {
-        32: "icons/cross-32.png",
-        48: "icons/cross-48.png"
+        32: 'icons/cross-32.png',
+        48: 'icons/cross-48.png'
       },
       tabId: currentTab.id
-    });
+    })
     browser.pageAction.setTitle({
       // Screen readers can see the title
       title: 'The icon is disabled. Enable it in the add-on settings.',
       tabId: currentTab.id
-    });
+    })
   }
 }
 
 /*
  * Add or remove the bookmark on the current page.
  */
-function toggleBookmark(tab) {
+function toggleBookmark (tab) {
   if (currentBookmark) {
-    browser.bookmarks.remove(currentBookmark.id);
+    browser.bookmarks.remove(currentBookmark.id)
   } else {
     // Create the bookmark (will toggle the handleCreated listener callback function)
-    browser.bookmarks.create({title: currentTab.title, url: currentTab.url});
+    browser.bookmarks.create({title: currentTab.title, url: currentTab.url})
   }
 }
 
 /*
  * Switches currentTab and currentBookmark to reflect the currently active tab
  */
-function updateActiveTab() {
-
-  function isSupportedProtocol(urlString) {
-    var supportedProtocols = ["https:", "http:"];
-    var url = document.createElement('a');
-    url.href = urlString;
-    return supportedProtocols.indexOf(url.protocol) != -1;
+function updateActiveTab () {
+  function isSupportedProtocol (urlString) {
+    let supportedProtocols = ['https:', 'http:']
+    let url = document.createElement('a')
+    url.href = urlString
+    return supportedProtocols.indexOf(url.protocol) !== -1
   }
 
-  function updateTab(tabs) {
-    var gettingOptions = browser.storage.local.get(OPTIONS_ARRAY);
+  function updateTab (tabs) {
+    let gettingOptions = browser.storage.local.get(OPTIONS_ARRAY)
     gettingOptions.then((options) => {
       if (tabs[0]) {
-        currentTab = tabs[0];
+        currentTab = tabs[0]
         if (quickBookmarkMode(options)) {
-          browser.pageAction.show(currentTab.id);
-          var searching;
+          browser.pageAction.show(currentTab.id)
+          let searching
           if (isSupportedProtocol(currentTab.url)) {
-            searching = browser.bookmarks.search({url: currentTab.url});
+            searching = browser.bookmarks.search({url: currentTab.url})
           } else {
-            searching = browser.bookmarks.search(currentTab.url);
+            searching = browser.bookmarks.search(currentTab.url)
           }
           searching.then((bookmarks) => {
             if (bookmarks.length === 1) {
-              currentBookmark = bookmarks[0];
+              currentBookmark = bookmarks[0]
               // Only proceed if bookmark matches current tab address
               if (currentBookmark.url === currentTab.url) {
                 if (inboxMode(options)) {
                   // Only keep current bookmark if it is in the default location
                   if (options[OPTIONS_FOLDER] === undefined || currentBookmark.parentId !== options[OPTIONS_FOLDER]) {
-                    currentBookmark = undefined;
+                    currentBookmark = undefined
                   }
                 }
               } else {
-                currentBookmark = undefined;
+                currentBookmark = undefined
               }
             } else if (bookmarks.length === 0) {
               // No bookmarks
-              currentBookmark = undefined;
+              currentBookmark = undefined
             } else if (bookmarks.length > 1) {
               // Duplicate bookmarks
-              currentBookmark = undefined;
-              browser.pageAction.hide(currentTab.id);
+              currentBookmark = undefined
+              browser.pageAction.hide(currentTab.id)
             }
-            updateIcon(true);
-          }, onError);
+            updateIcon(true)
+          }, onError)
         } else {
-          currentBookmark = undefined;
-          browser.pageAction.hide(currentTab.id);
-          updateIcon(false);
+          currentBookmark = undefined
+          browser.pageAction.hide(currentTab.id)
+          updateIcon(false)
         }
       }
-    }, onError);
+    }, onError)
   }
 
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-  gettingActiveTab.then(updateTab, onError);
+  let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true})
+  gettingActiveTab.then(updateTab, onError)
 }
 
 // Listen for clicks on the button
-browser.pageAction.onClicked.addListener(toggleBookmark);
+browser.pageAction.onClicked.addListener(toggleBookmark)
 
 // Listen for bookmarks being created
-browser.bookmarks.onCreated.addListener(updateActiveTab);
+browser.bookmarks.onCreated.addListener(updateActiveTab)
 
 // Listen for bookmarks being removed
-browser.bookmarks.onRemoved.addListener(updateActiveTab);
+browser.bookmarks.onRemoved.addListener(updateActiveTab)
 
 // Listen for bookmarks being moved
-browser.bookmarks.onMoved.addListener(updateActiveTab);
+browser.bookmarks.onMoved.addListener(updateActiveTab)
 
 // Listen to tab URL changes
-browser.tabs.onUpdated.addListener(updateActiveTab);
+browser.tabs.onUpdated.addListener(updateActiveTab)
 
 // Listen to tab switching
-browser.tabs.onActivated.addListener(updateActiveTab);
+browser.tabs.onActivated.addListener(updateActiveTab)
 
 // Listen for window switching
-browser.windows.onFocusChanged.addListener(updateActiveTab);
+browser.windows.onFocusChanged.addListener(updateActiveTab)
 
 // Update when the extension loads initially
-updateActiveTab();
+updateActiveTab()
--- options/options.js
+++ options/options.js
@@ -4,36 +4,35 @@
  * ================================================================================
  */
 
-var TAB_DEFAULT_NUMBER = 1;
+let TAB_DEFAULT_NUMBER = 1
 
-function tabManagement() {
-
-  var menuTabs = document.querySelectorAll('.tab_menu');
+function tabManagement () {
+  let menuTabs = document.querySelectorAll('.tab_menu')
 
   Array.from(menuTabs).forEach(link => {
     link.addEventListener('click', function (event) {
-      var tabNumber = this.dataset.tab;
-      switchTab(tabNumber);
-    });
-  });
+      let tabNumber = this.dataset.tab
+      switchTab(tabNumber)
+    })
+  })
 }
 
-function switchTab(number) {
-  var tabs = document.querySelectorAll('.tab_menu');
+function switchTab (number) {
+  let tabs = document.querySelectorAll('.tab_menu')
   Array.from(tabs).forEach(tabItem => {
-    tabItem.classList.remove('is-active');
-  });
-  document.querySelector("[data-tab='" + number + "']").classList.add('is-active');
+    tabItem.classList.remove('is-active')
+  })
+  document.querySelector("[data-tab='" + number + "']").classList.add('is-active')
 
-  var containers = document.querySelectorAll('.container_item');
+  let containers = document.querySelectorAll('.container_item')
   Array.from(containers).forEach(containerItem => {
-    containerItem.classList.remove('is-active');
-  });
-  document.querySelector("[data-item='" + number + "']").classList.add('is-active');
+    containerItem.classList.remove('is-active')
+  })
+  document.querySelector("[data-item='" + number + "']").classList.add('is-active')
 
   browser.storage.local.set({
     tab: number
-  }).then(null, onError);
+  }).then(null, onError)
 }
 
 /*
@@ -42,22 +41,22 @@ function switchTab(number) {
  * ================================================================================
  */
 
-const OPTIONS_FOLDER = "folder";
-const OPTIONS_OVERRIDE = "override";
-const OPTIONS_ICON = "icon";
-const OPTIONS_INBOX = "inbox";
-const OPTIONS_ADDTOTOP = "addtotop";
+const OPTIONS_FOLDER = 'folder'
+const OPTIONS_OVERRIDE = 'override'
+const OPTIONS_ICON = 'icon'
+const OPTIONS_INBOX = 'inbox'
+const OPTIONS_ADDTOTOP = 'addtotop'
 
-const QUERY_FOLDER = "#" + OPTIONS_FOLDER;
-const QUERY_OVERRIDE = "#" + OPTIONS_OVERRIDE;
-const QUERY_ICON = "#" + OPTIONS_ICON;
-const QUERY_INBOX = "#" + OPTIONS_INBOX;
-const QUERY_ADDTOTOP = "#" + OPTIONS_ADDTOTOP;
+const QUERY_FOLDER = '#' + OPTIONS_FOLDER
+const QUERY_OVERRIDE = '#' + OPTIONS_OVERRIDE
+const QUERY_ICON = '#' + OPTIONS_ICON
+const QUERY_INBOX = '#' + OPTIONS_INBOX
+const QUERY_ADDTOTOP = '#' + OPTIONS_ADDTOTOP
 
-const UNNAMED_FOLDER = "[no name]";
-const MISC_TAB = "tab";
+const UNNAMED_FOLDER = '[no name]'
+const MISC_TAB = 'tab'
 
-const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_INBOX, OPTIONS_ADDTOTOP, MISC_TAB];
+const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_INBOX, OPTIONS_ADDTOTOP, MISC_TAB]
 
 /*
  * ================================================================================
@@ -68,105 +67,103 @@ const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_I
 /*
  * Sets the selection option in a <select> element
  */
-function setOption(selectElement, value) {
+function setOption (selectElement, value) {
   return [...selectElement.options].some((option, index) => {
     if (option.value === value) {
-      selectElement.selectedIndex = index;
-      return true;
+      selectElement.selectedIndex = index
+      return true
     }
-  });
+  })
 }
 
 /*
  * Saves the options form
  */
-function saveOptions(e) {
+function saveOptions (e) {
   browser.storage.local.set({
     folder: document.querySelector(QUERY_FOLDER).value,
     override: document.querySelector(QUERY_OVERRIDE).checked,
     icon: document.querySelector(QUERY_ICON).checked,
     inbox: document.querySelector(QUERY_INBOX).checked,
     addtotop: document.querySelector(QUERY_ADDTOTOP).checked
-  }).then(null, onError);
+  }).then(null, onError)
 }
 
 /*
  * Restores the extension options
  */
-function restoreOptions() {
-
-  function updateOptions(bookmarkItems) {
-    buildTree(bookmarkItems);
-    var gettingOptions = browser.storage.local.get(OPTIONS_ARRAY);
+function restoreOptions () {
+  function updateOptions (bookmarkItems) {
+    buildTree(bookmarkItems)
+    let gettingOptions = browser.storage.local.get(OPTIONS_ARRAY)
     gettingOptions.then((res) => {
       if (res[OPTIONS_FOLDER] !== undefined) {
-        setOption(document.querySelector(QUERY_FOLDER), res[OPTIONS_FOLDER]);
+        setOption(document.querySelector(QUERY_FOLDER), res[OPTIONS_FOLDER])
       }
-      if (res[OPTIONS_OVERRIDE] !== undefined) document.querySelector(QUERY_OVERRIDE).checked = res[OPTIONS_OVERRIDE];
-      if (res[OPTIONS_ICON] !== undefined) document.querySelector(QUERY_ICON).checked = res[OPTIONS_ICON];
-      if (res[OPTIONS_INBOX] !== undefined) document.querySelector(QUERY_INBOX).checked = res[OPTIONS_INBOX];
+      if (res[OPTIONS_OVERRIDE] !== undefined) document.querySelector(QUERY_OVERRIDE).checked = res[OPTIONS_OVERRIDE]
+      if (res[OPTIONS_ICON] !== undefined) document.querySelector(QUERY_ICON).checked = res[OPTIONS_ICON]
+      if (res[OPTIONS_INBOX] !== undefined) document.querySelector(QUERY_INBOX).checked = res[OPTIONS_INBOX]
       if (res[OPTIONS_ADDTOTOP] !== undefined) document.querySelector(QUERY_ADDTOTOP).checked = res[OPTIONS_ADDTOTOP];
-      (res[MISC_TAB] !== undefined) ? switchTab(res[MISC_TAB]) : switchTab(TAB_DEFAULT_NUMBER);
-    }, onError);
+      (res[MISC_TAB] !== undefined) ? switchTab(res[MISC_TAB]) : switchTab(TAB_DEFAULT_NUMBER)
+    }, onError)
   }
 
-  var gettingTree = browser.bookmarks.getTree();
-  gettingTree.then(updateOptions, onError);
-
+  let gettingTree = browser.bookmarks.getTree()
+  gettingTree.then(updateOptions, onError)
 }
 
 /*
  * Adds an unbreakable space for indentation
  */
-function makeIndent(indentLength) {
-  return "\xA0\xA0".repeat(indentLength);
+function makeIndent (indentLength) {
+  return '\xA0\xA0'.repeat(indentLength)
 }
 
 /*
  * Builds the <select> options from the bookmarks tree
  */
-function buildItems(bookmarkItem, indent) {
+function buildItems (bookmarkItem, indent) {
   if (!bookmarkItem.url) {
     if (!bookmarkItem.title && indent === 0) {
       // Root of the bookmark tree
     } else {
-      var select = document.querySelector(QUERY_FOLDER);
-      var displayName;
+      let select = document.querySelector(QUERY_FOLDER)
+      let displayName
       if (!bookmarkItem.title) {
-        displayName = UNNAMED_FOLDER;
+        displayName = UNNAMED_FOLDER
       } else {
-        displayName = bookmarkItem.title;
+        displayName = bookmarkItem.title
       }
-      var key = makeIndent(indent) + displayName;
-      select.options[select.options.length] = new Option(key, bookmarkItem.id);
-      indent++;
+      let key = makeIndent(indent) + displayName
+      select.options[select.options.length] = new Option(key, bookmarkItem.id)
+      indent++
     }
   }
   if (bookmarkItem.children) {
-    for (var child of bookmarkItem.children) {
-      buildItems(child, indent);
+    for (let child of bookmarkItem.children) {
+      buildItems(child, indent)
     }
   }
-  indent--;
+  indent--
 }
 
 /*
  * Builds the bookmarks tree
  */
-function buildTree(bookmarkItems) {
-  buildItems(bookmarkItems[0], 0);
+function buildTree (bookmarkItems) {
+  buildItems(bookmarkItems[0], 0)
 }
 
 /*
  * Logs errors to the console
  */
-function onError(error) {
-  console.log(`An error occurred: ${error}`);
+function onError (error) {
+  console.log(`An error occurred: ${error}`)
 }
 
 // Listen for loading of the options page
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.addEventListener('DOMContentLoaded', tabManagement);
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.addEventListener('DOMContentLoaded', tabManagement)
 
 // Listen for saving of the options page
-document.querySelector("#tab_container").addEventListener("change", saveOptions);
+document.querySelector('#tab_container').addEventListener('change', saveOptions)
