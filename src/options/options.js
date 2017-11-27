cb8dccdf30c0e0683c253e3aa3039af03028a554
Unknown <dev@gustiaux.com>
2017-11-26 21:58:28 -0500

    Better handling of the new option page

--- src/options/options.js
+++ src/options/options.js
@@ -1,16 +1,183 @@
 /*
  * ================================================================================
- * TAB MANAGEMENT
+ * CONSTANTS
  * ================================================================================
  */
 
-let TAB_DEFAULT_NUMBER = 1
+// List of options IDs
+const OPT_FF_FOLDER = 'builtin-folder'
+const OPT_FF_TOP = 'builtin-top'
+const OPT_IC_ICON = 'icon-enabled'
+const OPT_IC_FOLDER = 'icon-folder'
+const OPT_IC_TOP = 'icon-top'
+const OPT_IC_INBOX = 'icon-inbox'
+
+// List of options query selectors
+const QRY_FF_FOLDER = '#' + OPT_FF_FOLDER
+const QRY_FF_TOP = '#' + OPT_FF_TOP
+const QRY_IC_ICON = '#' + OPT_IC_ICON
+const QRY_IC_FOLDER = '#' + OPT_IC_FOLDER
+const QRY_IC_TOP = '#' + OPT_IC_TOP
+const QRY_IC_INBOX = '#' + OPT_IC_INBOX
+
+// List of stored options properties
+const BUILTIN = 'builtin'
+const ICON = 'icon'
+const FOLDER = 'folder'
+const TOP = 'top'
+const ENABLED = 'enabled'
+const INBOX = 'inbox'
+
+// Miscellaneous
+const UNNAMED_FOLDER = '[no name]'
+const MISC_TAB = 'tab'
+const TAB_DEFAULT_NUMBER = 1
+
+// Allow to retrieve all stored options at once
+const OPTIONS_ARRAY = [BUILTIN, ICON, MISC_TAB]
+
+/*
+ * ================================================================================
+ * FUNCTIONS - OPTIONS
+ * ================================================================================
+ */
+
+/*
+ * Sets the selection option in a <select> element
+ */
+function setOption (selectElement, value) {
+  return [...selectElement.options].some((option, index) => {
+    if (option.value === value) {
+      selectElement.selectedIndex = index
+      return true
+    }
+  })
+}
+
+/*
+ * Toggles quick bookmark icon options depending on the feature status
+ */
+function toggleIconOptions (iconEnabled) {
+  if (iconEnabled !== undefined && iconEnabled === true) {
+    Array.from(document.querySelectorAll(':disabled')).forEach(item => {
+      item.removeAttribute('disabled')
+    })
+  } else {
+    document.querySelector(QRY_IC_FOLDER).setAttribute('disabled', '')
+    document.querySelector(QRY_IC_TOP).setAttribute('disabled', '')
+    document.querySelector(QRY_IC_INBOX).setAttribute('disabled', '')
+  }
+}
+/*
+ * Saves the options
+ */
+function saveOptions () {
+  let iconEnabled = document.querySelector(QRY_IC_ICON).checked
+  toggleIconOptions(iconEnabled)
+  let firefoxBookmarking = {
+    folder: document.querySelector(QRY_FF_FOLDER).value,
+    top: document.querySelector(QRY_FF_TOP).checked
+  }
+  let icon = {
+    enabled: iconEnabled,
+    folder: document.querySelector(QRY_IC_FOLDER).value,
+    top: document.querySelector(QRY_IC_TOP).checked,
+    inbox: document.querySelector(QRY_IC_INBOX).checked
+  }
+  browser.storage.local.set({
+    builtin: firefoxBookmarking,
+    icon: icon
+  }).then(null, onError)
+}
+
+/*
+ * Restores the extension options
+ */
+function restoreOptions () {
+  function updateOptions (bookmarkItems) {
+    buildTree(bookmarkItems, [QRY_FF_FOLDER, QRY_IC_FOLDER])
+    let gettingOptions = browser.storage.local.get(OPTIONS_ARRAY)
+    gettingOptions.then((res) => {
+      // For Firefox built-in bookmarking
+      if (res[BUILTIN][FOLDER] !== undefined) setOption(document.querySelector(QRY_FF_FOLDER), res[BUILTIN][FOLDER])
+      if (res[BUILTIN][TOP] !== undefined) document.querySelector(QRY_FF_TOP).checked = res[BUILTIN][TOP]
+      // For quick bookmark icon
+      if (res[ICON][ENABLED] !== undefined) document.querySelector(QRY_IC_ICON).checked = res[ICON][ENABLED]
+      if (res[ICON][FOLDER] !== undefined) setOption(document.querySelector(QRY_IC_FOLDER), res[ICON][FOLDER])
+      if (res[ICON][TOP] !== undefined) document.querySelector(QRY_IC_TOP).checked = res[ICON][TOP]
+      if (res[ICON][INBOX] !== undefined) document.querySelector(QRY_IC_INBOX).checked = res[ICON][INBOX]
+      toggleIconOptions(res[ICON][ENABLED])
+      // For tab management
+      res[MISC_TAB] !== undefined ? switchTab(res[MISC_TAB]) : switchTab(TAB_DEFAULT_NUMBER)
+    }, onError)
+  }
+
+  let gettingTree = browser.bookmarks.getTree()
+  gettingTree.then(updateOptions, onError)
+}
+
+/*
+ * Adds an unbreakable space for indentation
+ */
+function makeIndent (indentLength) {
+  return '\xA0\xA0'.repeat(indentLength)
+}
+
+/*
+ * Builds the <select> options from the bookmarks tree
+ */
+function buildItems (bookmarkItem, indent, selectors) {
+  if (!bookmarkItem.url) {
+    if (!bookmarkItem.title && indent === 0) {
+      // Root of the bookmark tree
+    } else {
+      let displayName
+      let select
+      if (!bookmarkItem.title) {
+        displayName = UNNAMED_FOLDER
+      } else {
+        displayName = bookmarkItem.title
+      }
+      let key = makeIndent(indent) + displayName
+      Array.from(selectors).forEach(selector => {
+        select = document.querySelector(selector)
+        select.options[select.options.length] = new Option(key, bookmarkItem.id)
+      })
+      indent++
+    }
+  }
+  if (bookmarkItem.children) {
+    for (let child of bookmarkItem.children) {
+      buildItems(child, indent, selectors)
+    }
+  }
+  indent--
+}
+
+/*
+ * Builds the bookmarks tree
+ */
+function buildTree (bookmarkItems, selectors) {
+  buildItems(bookmarkItems[0], 0, selectors)
+}
+
+/*
+ * Logs errors to the console
+ */
+function onError (error) {
+  console.log(`An error occurred: ${error}`)
+}
+
+/*
+ * ================================================================================
+ * FUNCTIONS - TAB MANAGEMENT
+ * ================================================================================
+ */
 
 function tabManagement () {
   let menuTabs = document.querySelectorAll('.tab_menu')
-
   Array.from(menuTabs).forEach(link => {
-    link.addEventListener('click', function (event) {
+    link.addEventListener('click', function () {
       let tabNumber = this.dataset.tab
       switchTab(tabNumber)
     })
@@ -37,129 +204,9 @@ function switchTab (number) {
 
 /*
  * ================================================================================
- * CONSTANTS
+ * LISTENERS
  * ================================================================================
  */
-
-const OPTIONS_FOLDER = 'builtin-folder'
-const OPTIONS_OVERRIDE = 'override'
-const OPTIONS_ICON = 'icon'
-const OPTIONS_INBOX = 'inbox'
-const OPTIONS_ADDTOTOP = 'addtotop'
-
-const QUERY_FOLDER = '#' + OPTIONS_FOLDER
-const QUERY_OVERRIDE = '#' + OPTIONS_OVERRIDE
-const QUERY_ICON = '#' + OPTIONS_ICON
-const QUERY_INBOX = '#' + OPTIONS_INBOX
-const QUERY_ADDTOTOP = '#' + OPTIONS_ADDTOTOP
-
-const UNNAMED_FOLDER = '[no name]'
-const MISC_TAB = 'tab'
-
-const OPTIONS_ARRAY = [OPTIONS_FOLDER, OPTIONS_OVERRIDE, OPTIONS_ICON, OPTIONS_INBOX, OPTIONS_ADDTOTOP, MISC_TAB]
-
-/*
- * ================================================================================
- * FUNCTIONS
- * ================================================================================
- */
-
-/*
- * Sets the selection option in a <select> element
- */
-function setOption (selectElement, value) {
-  return [...selectElement.options].some((option, index) => {
-    if (option.value === value) {
-      selectElement.selectedIndex = index
-      return true
-    }
-  })
-}
-
-/*
- * Saves the options form
- */
-function saveOptions (e) {
-  browser.storage.local.set({
-    folder: document.querySelector(QUERY_FOLDER).value,
-    override: document.querySelector(QUERY_OVERRIDE).checked,
-    icon: document.querySelector(QUERY_ICON).checked,
-    inbox: document.querySelector(QUERY_INBOX).checked,
-    addtotop: document.querySelector(QUERY_ADDTOTOP).checked
-  }).then(null, onError)
-}
-
-/*
- * Restores the extension options
- */
-function restoreOptions () {
-  function updateOptions (bookmarkItems) {
-    buildTree(bookmarkItems)
-    let gettingOptions = browser.storage.local.get(OPTIONS_ARRAY)
-    gettingOptions.then((res) => {
-      if (res[OPTIONS_FOLDER] !== undefined) {
-        setOption(document.querySelector(QUERY_FOLDER), res[OPTIONS_FOLDER])
-      }
-      if (res[OPTIONS_OVERRIDE] !== undefined) document.querySelector(QUERY_OVERRIDE).checked = res[OPTIONS_OVERRIDE]
-      if (res[OPTIONS_ICON] !== undefined) document.querySelector(QUERY_ICON).checked = res[OPTIONS_ICON]
-      if (res[OPTIONS_INBOX] !== undefined) document.querySelector(QUERY_INBOX).checked = res[OPTIONS_INBOX]
-      if (res[OPTIONS_ADDTOTOP] !== undefined) document.querySelector(QUERY_ADDTOTOP).checked = res[OPTIONS_ADDTOTOP];
-      (res[MISC_TAB] !== undefined) ? switchTab(res[MISC_TAB]) : switchTab(TAB_DEFAULT_NUMBER)
-    }, onError)
-  }
-
-  let gettingTree = browser.bookmarks.getTree()
-  gettingTree.then(updateOptions, onError)
-}
-
-/*
- * Adds an unbreakable space for indentation
- */
-function makeIndent (indentLength) {
-  return '\xA0\xA0'.repeat(indentLength)
-}
-
-/*
- * Builds the <select> options from the bookmarks tree
- */
-function buildItems (bookmarkItem, indent) {
-  if (!bookmarkItem.url) {
-    if (!bookmarkItem.title && indent === 0) {
-      // Root of the bookmark tree
-    } else {
-      let select = document.querySelector(QUERY_FOLDER)
-      let displayName
-      if (!bookmarkItem.title) {
-        displayName = UNNAMED_FOLDER
-      } else {
-        displayName = bookmarkItem.title
-      }
-      let key = makeIndent(indent) + displayName
-      select.options[select.options.length] = new Option(key, bookmarkItem.id)
-      indent++
-    }
-  }
-  if (bookmarkItem.children) {
-    for (let child of bookmarkItem.children) {
-      buildItems(child, indent)
-    }
-  }
-  indent--
-}
-
-/*
- * Builds the bookmarks tree
- */
-function buildTree (bookmarkItems) {
-  buildItems(bookmarkItems[0], 0)
-}
-
-/*
- * Logs errors to the console
- */
-function onError (error) {
-  console.log(`An error occurred: ${error}`)
-}
 
 // Listen for loading of the options page
 document.addEventListener('DOMContentLoaded', restoreOptions)
