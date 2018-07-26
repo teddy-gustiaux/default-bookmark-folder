6c17448b980e1da08b8781e1bbe74896f70494a9
Teddy Gustiaux <code@gustiaux.com>
2018-07-26 18:07:38 -0400

    Use mocha reporter instead of default progress reporter

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'html'],
+    reporters: ['mocha', 'html'],
     htmlReporter: {
       outputFile: 'karma/results.html',
       pageTitle: 'Default Bookmark Folder',
