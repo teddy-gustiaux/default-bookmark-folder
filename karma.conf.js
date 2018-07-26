a56d10086170ec26ae62989c17ab4546a52cad7e
Teddy Gustiaux <code@gustiaux.com>
2018-07-26 17:33:19 -0400

    Update Karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,47 @@
 // Karma configuration
-// Generated on Tue Jul 24 2018 19:51:12 GMT-0400 (Eastern Summer Time)
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs', 'chai'],
-
-
+    frameworks: [
+      'mocha',
+      //'requirejs'
+      'chai',
+      'dirty-chai',
+    ],
     // list of files / patterns to load in the browser
     files: [
+      // Dependencies
+      "node_modules/sinon/pkg/sinon.js",
+      'node_modules/sinon-chrome/bundle/sinon-chrome.min.js',
+      // Sources
       'src/background/utils/Utils.js',
-      'test/test-main.js',
-      { pattern: 'test/**/*.js', included: false }
+      // Test files
+      //'test/test-main.js',
+      { pattern: 'test/**/*.test.js', included: true }
     ],
-
-
     // list of files / patterns to exclude
     exclude: [
       '**/*.html',
       '**/*.png',
       '**/*.css'
     ],
-
-
+    client: {
+      mocha: {
+        // change Karma's debug.html to the mocha web reporter
+        reporter: 'html',
+      }
+    },
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
-
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'html'],
-
-
     htmlReporter: {
       outputFile: 'karma/results.html',
       pageTitle: 'Default Bookmark Folder',
@@ -49,44 +50,29 @@ module.exports = function(config) {
       useCompactStyle: true,
       useLegacyStyle: false,  
     },
-
-
     // web server port
     port: 9876,
-
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
-
-
+    logLevel: config.LOG_ERROR,
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['FirefoxHeadless'],
-
-
     customLaunchers: {
       FirefoxHeadless: {
         base: 'Firefox',
         flags: [ '-headless' ],
       },
     },
-
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
+    singleRun: true,
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: 1
   })
 }
