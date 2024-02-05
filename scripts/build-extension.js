818868e949c810049ad5deacc3cf27f22759738c
Teddy Gustiaux <code@gustiaux.com>
2024-02-04 19:43:28 -0500

    chore: update addon building and debugging tooling

---
+++ scripts/build-extension.js
@@ -0,0 +1,18 @@
+import * as path from 'node:path';
+import webExt from 'web-ext';
+import { buildManifest } from './helpers/ManifestBuilder.js';
+
+const target = process.argv[2];
+const { browser, sourceDir } = await buildManifest(target);
+console.log(path.join(sourceDir.replace('src', 'dist'), browser));
+
+webExt.cmd.build(
+	{
+		sourceDir,
+		overwriteDest: true,
+		artifactsDir: path.join(sourceDir.replace('src', 'dist'), browser),
+	},
+	{
+		shouldExitProgram: true,
+	},
+);
---
+++ scripts/start-browser.js
@@ -0,0 +1,30 @@
+import webExt from 'web-ext';
+import { ManifestBuilder, buildManifest } from './helpers/ManifestBuilder.js';
+
+const target = process.argv[2];
+const { browser, sourceDir } = await buildManifest(target);
+
+let specificOptions = {};
+if (browser === ManifestBuilder.FIREFOX) {
+	specificOptions = {
+		firefox: target,
+	};
+} else {
+	specificOptions = {
+		target: 'chromium',
+	};
+}
+
+webExt.cmd.run(
+	{
+		...specificOptions,
+		sourceDir,
+		devtools: true,
+		pref: {
+			'browser.toolbars.bookmarks.visibility': 'always',
+		},
+	},
+	{
+		shouldExitProgram: true,
+	},
+);
